cd57832a7de3196eeb627bcd4d0b5422b26c043a
Shitao Tang <tst17@my.swjtu.edu.cn>
2025-04-21 18:40:00 +0800

    fix bugs on aarch64

--- src/thread/aarch64/clone.rs
+++ src/thread/aarch64/clone.rs
@@ -1,42 +1,71 @@
-use crate::include::ctype::*;
-use core::arch::asm;
+// use crate::include::ctype::*;
+// use core::arch::asm;
 
-#[inline(always)]
-#[no_mangle]
-pub unsafe extern "C" fn __clone(
-    _func: unsafe extern "C" fn(*mut c_void) -> c_int,
-    _stack: *mut c_void,
-    _flags: c_int,
-    _args: *mut c_void,
-    _ptid: *mut c_int,
-    _tls: *mut c_void,
-    _ctid: *mut c_int
-) -> c_int {
-    let ret: c_int;
-    asm!(
-        "and x1, x1, 0xfffffffffffffff0",
-        "stp x0, x3, [x1, 0xfffffffffffffff0]!",
+// #[inline(always)]
+// #[no_mangle]
+// pub unsafe extern "C" fn __clone(
+//     _func: unsafe extern "C" fn(*mut c_void) -> c_int,
+//     _stack: *mut c_void,
+//     _flags: c_int,
+//     _args: *mut c_void,
+//     _ptid: *mut c_int,
+//     _tls: *mut c_void,
+//     _ctid: *mut c_int
+// ) -> c_int {
+//     let ret: c_int;
+//     asm!(
+//         "and x1, x1, 0xfffffffffffffff0",
+//         "stp x0, x3, [x1, 0xfffffffffffffff0]!",
 
-        "uxtw x0, w2",
-        "mov x2, x4",
-        "mov x3, x5",
-        "mov x4, x6",
-        "mov x8, 220",
-        "svc 0",
+//         "uxtw x0, w2",
+//         "mov x2, x4",
+//         "mov x3, x5",
+//         "mov x4, x6",
+//         "mov x8, 220",
+//         "svc 0",
 
-        "cbz x0, 2f",
-        "b 3f",
+//         "cbz x0, 2f",
+//         "b 3f",
 
-        "2:",
-        "ldp x1, x0, [sp], 16",
-        "blr x1",
-        "mov x8, 93",
-        "svc 0",
+//         "2:",
+//         "ldp x1, x0, [sp], 16",
+//         "blr x1",
+//         "mov x8, 93",
+//         "svc 0",
 
-        "3:",
+//         "3:",
 
-        out ("x0") ret,   // x0
-        clobber_abi("C"),  // clobber_abi
-    );
+//         out ("x0") ret,   // x0
+//         clobber_abi("C"),  // clobber_abi
+//     );
+//     ret
+// }
+
+use core::arch::global_asm;
+
+global_asm!(
+    r#"
+.global __clone
+.type __clone, %function
+__clone:
+    and x1, x1, 0xfffffffffffffff0
+    stp x0, x3, [x1, 0xfffffffffffffff0]!
+
+    uxtw x0, w2
+    mov x2, x4
+    mov x3, x5
+    mov x4, x6
+    mov x8, 220
+    svc 0
+
+    cbz x0, 2f
+    // parent
     ret
-}
+    // child
+2:
+    ldp x1, x0, [sp], 16
+    blr x1
+    mov x8, 93
+    svc 0
+    "#
+);
--- src/thread/aarch64/syscall_cp.rs
+++ src/thread/aarch64/syscall_cp.rs
@@ -1,52 +1,80 @@
-use core::arch::asm;
-use super::super::pthread_cancel::cancel;
+// use core::arch::asm;
+// use super::super::pthread_cancel::cancel;
 
-#[inline(always)]
-#[no_mangle]
-pub unsafe extern "C" fn __syscall_cp_asm(
-    cancel_ptr: *const i32,
-    nr: i64,
-    u: i64,
-    v: i64,
-    w: i64,
-    x: i64,
-    y: i64,
-    z: i64,
-) -> i64 {
-    let result: i64;
-    asm!(
-        // __cp_begin
-        "ldr w0, [{cancel_ptr}]",
-        "cbnz w0, 2f",
-        "mov x8, {nr}",
-        "mov x0, {u}",
-        "mov x1, {v}",
-        "mov x2, {w}",
-        "mov x3, {x}",
-        "mov x4, {y}",
-        "mov x5, {z}",
-        "svc 0",
-        "b 3f",
+// #[inline(always)]
+// #[no_mangle]
+// pub unsafe extern "C" fn __syscall_cp_asm(
+//     cancel_ptr: *const i32,
+//     nr: i64,
+//     u: i64,
+//     v: i64,
+//     w: i64,
+//     x: i64,
+//     y: i64,
+//     z: i64,
+// ) -> i64 {
+//     let result: i64;
+//     asm!(
+//         // __cp_begin
+//         "ldr w0, [{cancel_ptr}]",
+//         "cbnz w0, 2f",
+//         "mov x8, {nr}",
+//         "mov x0, {u}",
+//         "mov x1, {v}",
+//         "mov x2, {w}",
+//         "mov x3, {x}",
+//         "mov x4, {y}",
+//         "mov x5, {z}",
+//         "svc 0",
+//         "b 3f",
 
-        // __cp_cancel
-        "2:",
-        "b {cancel}",
+//         // __cp_cancel
+//         "2:",
+//         "b {cancel}",
 
-        // __cp_end
-        "3:",
+//         // __cp_end
+//         "3:",
 
-        cancel_ptr = in(reg) cancel_ptr,
-        nr = in(reg) nr,
-        u = in(reg) u,
-        v = in(reg) v,
-        w = in(reg) w,
-        x = in(reg) x,
-        y = in(reg) y,
-        z = in(reg) z,
+//         cancel_ptr = in(reg) cancel_ptr,
+//         nr = in(reg) nr,
+//         u = in(reg) u,
+//         v = in(reg) v,
+//         w = in(reg) w,
+//         x = in(reg) x,
+//         y = in(reg) y,
+//         z = in(reg) z,
 
-        cancel = sym cancel,
-        out("x0") result,
-        clobber_abi("C"),
-    );
-    result
-}
+//         cancel = sym cancel,
+//         out("x0") result,
+//         clobber_abi("C"),
+//     );
+//     result
+// }
+
+use core::arch::global_asm;
+
+global_asm!(
+    r#"
+.global __cp_begin
+.global __cp_cancel
+.global __cp_end
+.global __syscall_cp_asm
+.type   __syscall_cp_asm, %function
+__syscall_cp_asm:
+__cp_begin:
+    ldr w0, [x0]
+    cbnz w0, __cp_cancel
+    mov x8, x1
+    mov x0, x2
+    mov x1, x3
+    mov x2, x4
+    mov x3, x5
+    mov x4, x6
+    mov x5, x7
+    svc 0
+__cp_end:
+    ret
+__cp_cancel:
+    b __cancel
+    "#
+);
